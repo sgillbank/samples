3bde6e23bb978cc3dad6b291a5200c5f9b3a159f
michael-shi <michael.shi@nodus.com>
2014-05-23 08:41:21 +0800

    Create .net sample code structure

---
+++ Net40/Address/RetrieveByCustomer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PayFabric.Samples.Net40.Address
+{
+    class RetrieveByCustomer
+    {
+    }
+}
