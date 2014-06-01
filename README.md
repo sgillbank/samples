7585d2ee7f43a41e1d6ac83465d9aa99927ca9b3
michael-shi <michael.shi@nodus.com>
2014-06-02 05:55:39 +0800

    new folders

--- README.md
+++
@@ -1,4 +0,0 @@
-samples
-=======
-
-PayFabric Sample Code
--- Samples/net40/Properties/AssemblyInfo.cs
+++ Samples/net40/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Net40")]
+[assembly: AssemblyTitle("net40")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Net40")]
+[assembly: AssemblyProduct("net40")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e4c4a2a1-9620-4328-8479-9cfe238e59f2")]
+[assembly: Guid("183b4401-96d8-4501-9f98-90c87eacefe7")]
 
 // Version information for an assembly consists of the following four values:
 //
